05140f8f6431481efe2a0f2aa7854848d9a3f796
bjnzoom2 <bjnzoom2@gmail.com>
2026-03-10 20:50:36 +0800

    More Minor Changes

--- vertex.glsl
+++ vertex.glsl
@@ -12,5 +12,5 @@ uniform mat4 model;
 void main() {
 	gl_Position = projection * view * model * vec4(aPos, 1.0f);
 	FragPos = vec3(model * vec4(aPos, 1.0));
-	normal = aNormal;
+	normal = mat3(transpose(inverse(model))) * aNormal;
 }
