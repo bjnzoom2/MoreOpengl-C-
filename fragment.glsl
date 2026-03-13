f63ace86eb58d424dd50f484e9fdcf7f9caa5867
bjnzoom2 <bjnzoom2@gmail.com>
2026-03-13 12:17:36 +0800

    Specular lighting fix

--- fragment.glsl
+++ fragment.glsl
@@ -19,9 +19,10 @@ vec3 diffuse = diff * lightColor;
 vec3 viewDir = normalize(viewPos - FragPos);
 vec3 reflectDir = reflect(-lightDir, norm);  
 float spec = pow(max(dot(viewDir, reflectDir), 0.0), 16);
-vec3 specular = specularStrength * spec * lightColor;  
-vec3 result = (ambient + diffuse + specular) * color;
+vec3 specular = specularStrength * spec * lightColor;
 
 void main() {
+	specular *= (diff > 0.0 ? 1.0 : 0.0);
+	vec3 result = (ambient + diffuse + specular) * color;
 	FragColor = vec4(result, 1.0f);
 }
